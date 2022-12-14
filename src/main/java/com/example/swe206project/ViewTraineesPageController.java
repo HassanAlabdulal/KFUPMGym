8774f5f8ebc6ff951ce50df838a504fd8f9309e4
l3b3al <alabdulaal.alii@gmail.com>
2022-12-14 18:17:56 +0300

    view trainees buttons design

--- src/main/java/com/example/swe206project/ViewTraineesPageController.java
+++ src/main/java/com/example/swe206project/ViewTraineesPageController.java
@@ -8,7 +8,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.Glow;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ViewTraineesPageController implements Initializable {
 
@@ -29,6 +31,7 @@ public class ViewTraineesPageController implements Initializable {
 
     @FXML
     private TableView<Trainee> viewTraineesTable;
+
     @FXML
     public TableColumn<Trainee, String> name;
 
@@ -45,18 +48,10 @@ public class ViewTraineesPageController implements Initializable {
     private TableColumn<Trainee, String> status;
 
     @FXML
-    Trainer trainer = (Trainer) LoginFormController.user;
-
-    @FXML
-   ObservableList<Trainee> list = FXCollections.observableArrayList();
-
-
-   //ObservableList<Trainee> list = FXCollections.observableArrayList();
+    private Trainer trainer = (Trainer) LoginFormController.user;
 
     @FXML
     public void initialize() {
-        //Trainer trainer = (Trainer) LoginFormController.user;
-        //ObservableList<Trainee> list = FXCollections.observableArrayList(trainer.getTraineesList());
         
         name.setCellValueFactory(new PropertyValueFactory<Trainee, String>("name"));
         height.setCellValueFactory(new PropertyValueFactory<Trainee, Double>("height"));
@@ -64,8 +59,90 @@ public class ViewTraineesPageController implements Initializable {
         plan.setCellValueFactory(new PropertyValueFactory<Trainee, Plan>("plan"));
         status.setCellValueFactory(new PropertyValueFactory<Trainee, String>("status"));
        
-        //viewTraineesTable.setItems(FXCollections.observableArrayList(trainer.getTraineesListTest()));
         viewTraineesTable.setItems(trainer.getTest());
+    }
+
+    // Add new trainee button design
+    @FXML
+    void addNewTraineeOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addNewTraineeOnPressed(MouseEvent event) {
+        addNewTrainee.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+        addNewTrainee.setEffect(new Glow(0.3));
+    }
+
+    @FXML
+    void addNewTraineeOnReleased(MouseEvent event) {
+        addNewTrainee.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15; -fx-cursor: hand");
+        addNewTrainee.setEffect(new Glow(0.0));
+    }
+
+    @FXML
+    void addNewTraineeInHover(MouseEvent event) {
+        addNewTrainee.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+    }
+
+    @FXML
+    void addNewTraineeOutHover(MouseEvent event) {
+        addNewTrainee.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15");
+    }
+
+    // Assign plan button design
+    @FXML
+    void assignPlanOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void assignPlanOnPressed(MouseEvent event) {
+        assignPlan.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+        assignPlan.setEffect(new Glow(0.3));
+    }
+
+    @FXML
+    void assignPlanOnReleased(MouseEvent event) {
+        assignPlan.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15; -fx-cursor: hand");
+        assignPlan.setEffect(new Glow(0.0));
+    }
+
+    @FXML
+    void assignPlanInHover(MouseEvent event) {
+        assignPlan.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+    }
+
+    @FXML
+    void assignPlanOutHover(MouseEvent event) {
+        assignPlan.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15");
+    }
+
+    // Progress button style
+    @FXML
+    void progressPlanOnClicked(MouseEvent event) {
+
+    }
+    @FXML
+    void progressOnPressed(MouseEvent event) {
+        progress.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+        progress.setEffect(new Glow(0.3));
+    }
+
+    @FXML
+    void progressOnReleased(MouseEvent event) {
+        progress.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15; -fx-cursor: hand");
+        progress.setEffect(new Glow(0.0));
+    }
+
+    @FXML
+    void progressInHover(MouseEvent event) {
+        progress.setStyle("-fx-background-color: #366D55; -fx-background-radius: 15; -fx-cursor: hand");
+    }
+
+    @FXML
+    void progressOutHover(MouseEvent event) {
+        progress.setStyle("-fx-background-color: #43896B; -fx-background-radius: 15");
     }
 
 }
