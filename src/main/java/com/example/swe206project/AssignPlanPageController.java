a4dbae7e87c2b871e9d0cf71781c140dd3145d38
l3b3al <alabdulaal.alii@gmail.com>
2022-12-16 18:34:08 +0300

    bug in assign plan initializable method

--- src/main/java/com/example/swe206project/AssignPlanPageController.java
+++ src/main/java/com/example/swe206project/AssignPlanPageController.java
@@ -1,5 +1,71 @@
 package com.example.swe206project;
 
-public class AssignPlanPageController {
-    
+import javax.security.auth.callback.Callback;
+
+import javafx.collections.FXCollections;
+import javafx.css.converter.StringConverter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class AssignPlanPageController implements Initializable {
+
+    @FXML
+    private Button assignPlan;
+
+    @FXML
+    private ImageView back;
+
+    @FXML
+    private ImageView confirmIcon;
+
+    @FXML
+    private Button customizePlan;
+
+    @FXML
+    private ImageView errorIcon;
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private ComboBox<Plan> predefinedPlans;
+
+    @FXML
+    public void initialize() {
+        predefinedPlans.setItems(FXCollections.observableArrayList());
+
+        predefinedPlans.setCellFactory(new Callback<ListView<Plan>, ListCell<Plan>>() {
+            @Override
+            public ListCell<Plan> call(ListView<Plan> param) {
+                return new ListCell<Plan>() {
+                        @Override 
+                        protected void updateItem(Plan plan, boolean empty) {
+                            super.updateItem(plan, empty);
+                            
+                            if (plan == null || empty) {
+                                setText(null);
+                            } else { 
+                                setText(plan.getPlanName()); //Set the text to be displayed as the name property of your object.            
+                            }                   
+                        }  
+                };
+            }
+        });
+
+        predefinedPlans.setConverter(new StringConverter<Plan>() {
+            @Override
+            public String toString(Plan plan) {
+              if (plan == null){
+                return null;
+              } else {
+                return plan.getPlanName();
+              }
+            }
+      });
+    }
 }
