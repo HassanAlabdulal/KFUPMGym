19cf9aec7e0a6461d13d822cc45c136e5932f0b8
Ahmed <ahmedabdulaal746@gmail.com>
2022-12-18 00:03:09 +0300

    gg commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+To peform and achive this project, the team used VS code to code the software and scene builder to create the UI for it. 
+It should work fine with any new machines, Just import the project files and run it 
--- src/main/java/com/example/swe206project/SplashScreenController.java
+++ src/main/java/com/example/swe206project/SplashScreenController.java
@@ -40,7 +40,7 @@ public class SplashScreenController implements Initializable {
         @Override
         public void run() {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
